5bc7aef851348f8fcf19d16227b2ad16f13613c3
MahmoudBen1 <>
2021-05-05 19:15:53 +0000

    add JS

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+var user = prompt('Whats your name?')
+
+alert('lets Go');
+
+
+var sport = prompt('Whats your favourite sport?')
+
+alert('lets Go');
+
+if(sport === 'snowboarding'){
+ window.location = "https://www.youtube.com/watch?v=t705_V-RDcQ";
+}else if(sport === 'japanese sword game'){
+  window.location = 'https://www.youtube.com/watch?v=8o5Y1OLXPh0'
+  }
+  else if(sport === 'soccer'){
+    window.location = 'https://www.youtube.com/watch?v=L4aBqvbP_MM'
+}
+else {
+  alert('okay you should only pick snowboarding or japanese sword game or soccer')
+}
+
+
